fe242b0c070b23a178e27371b3c076022af20dab
Mao Rivera <maorivera@outlook.com>
2020-04-28 11:01:59 -0500

    Iniciando proyecto

---
+++ src/almacen_teleton/Inicio.java
@@ -0,0 +1,17 @@
+
+package almacen_teleton;
+
+/**
+ *
+ * @author maori
+ */
+public class Inicio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
